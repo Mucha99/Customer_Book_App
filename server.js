9c955d6dadd7053bb19eee2051beb6e00e3346a7
Bartek Mucha <bartekmucha43@gmail.com>
2021-07-27 21:31:34 +0200

    Add fils for Heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const testimonials = require('./routes/testimonials.routes');
 const concerts = require('./routes/concerts.routes');
@@ -7,14 +8,17 @@ const seats = require('./routes/seats.routes');
 
 const app = express();
 
-app.listen(8000, () => {
-    console.log('Server is running on port: 8000');
-});
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 // middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
 
 // endpoints
 app.use('/api', testimonials);
@@ -25,3 +29,7 @@ app.use('/api', seats);
 app.use((req, res) => {
     res.status(404).json({ message: 'Not found...'})
 })
+
+app.listen(process.env.PORT || 8000, () => {
+    console.log('Server is running on port: 8000');
+});
